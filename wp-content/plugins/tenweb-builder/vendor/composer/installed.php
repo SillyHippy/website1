2d5685e3346fe0086742ec8e8a95306be92de79d
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-03-09 22:46:41 -0500

    33

---
+++ wp-content/plugins/tenweb-builder/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'plugins/tenweb-builder',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '00a6409958a45d9e50e4d90e3eaac61bb6784f3a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '10web/authorization' => array(
+            'pretty_version' => 'dev-dev',
+            'version' => 'dev-dev',
+            'reference' => 'fe0f0847cf16f5ed82fa62539744b8a51edf8916',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../10web/authorization',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'plugins/tenweb-builder' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '00a6409958a45d9e50e4d90e3eaac61bb6784f3a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
